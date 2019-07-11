7c48245c27d2ed9e7f8df952db51d08e41cea0dd
Stephen <48072084+StephenDaDev@users.noreply.github.com>
2019-07-11 14:50:46 -0400

    me dumb dumb dumb dumb

---
+++ poller/poller.py
@@ -0,0 +1,25 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+Cog = getattr(commands, "Cog", object)
+
+
+class Autorole(Cog):
+    """DO NOT EAT MY MODMAIL PLUGIN UNDER ANY CIRCUMSTANCES
+    """
+
+
+@commands.command(pass_context=True)
+@checks.has_permissions(PermissionLevel.SUPPORTER)
+async def emoji(ctx):
+    msg = await bot.say("working")
+    reactions = ['dart']
+    for emoji in reactions: 
+        await bot.add_reaction(msg, emoji)
+        
+        def setup(bot):
+    bot.add_cog(Autorole(bot))
